46d69b5f41ea064bc34ded9a6b5dbc021a1f1fa0
Rafi Syahrulfslsh <rafisyahrulfalah@gmail.com>
2024-11-26 23:28:32 +0700

    Delete latihan directory

--- latihan/getMobil.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = new mysqli('localhost', 'root', '', 'rental_mobil');
-$no_plat = $_GET['no_plat'];
-$result = $conn->query("SELECT * FROM kendaraan WHERE no_plat = '$no_plat'");
-echo json_encode($result->fetch_assoc());
-$conn->close();
-?>
--- latihan/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_unset(); // Menghapus semua variabel sesi
-session_destroy(); // Menghancurkan sesi
-header("Location: formlogin.php"); // Mengarahkan kembali ke halaman login
-exit;
-?>
